f3f60d503a24fc8c245e39d7e00505532c91ef01
Parvez Khan Pathan <41503632+iamParvezKhan25@users.noreply.github.com>
2020-01-07 14:57:58 +0530

    Add files via upload

---
+++ mysite/blog/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.user',on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    '''
+    # get_absolute_url :
+
+    Define a get_absolute_url() method to tell Django how to calculate the canonical URL for an object.
+    To callers, this method should appear to return a string that can be used to refer to the object over HTTP.
+
+    # reverse :
+    after someone create 'Post' or 'comment' where should the website take them.
+    '''
+    def get_absolute_url(self):
+        return reverse("post_detail",kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,related_name='comments',on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    approved_comments = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comments = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("post_list")
+
+    def __str__(self):
+        return self.text
